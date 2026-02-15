29e55c0a43593bea2a529aba693ac1cccdcd30d5
Warunyu17 <waranyoo2830@hotmail.com>
2026-02-15 21:11:45 +0700

    แก้ mongo_url

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/move-for-health';
 
 if (!MONGODB_URI) {
     throw new Error(
